a3100814005a8a2cdab18b80f6f62aac4d737f5d
龍成 小林 <190736@jc-21.jp>
2022-02-04 16:04:15 +0900

    プロジェクト ファイルを追加します。

---
+++ JetCoaster.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_10
+{
+    public class JetCoaster
+    {
+
+       public static int Ride(int k,int[] g,int r)
+        {
+
+            int ninzu = 0; //人数
+            int a = k;//現在人数制限
+
+            for (int j = 0; ; j += 1)//配列カウント
+                {
+                
+                if(r == 0)//rまで回す
+                {
+                    break;
+                }
+
+                if (j > g.Length - 1)//配列上限まで行ったら最初に戻る
+                {
+                    j = 0;
+                }
+                Console.WriteLine("g"+g[j]);
+                if (g[j] <= a)//人数はいるとき
+                {
+                    ninzu += g[j];
+                    a -= g[j];
+                }
+
+                else//人数はいらないとき
+                {
+
+                    a = k;
+                    r -= 1;
+
+                    if (j <= g.Length - 1)
+                    {
+                        j -= 1;
+                    }
+                    else
+                    {
+                        j = g.Length - 1;
+                    }
+                }
+
+            }
+
+             return ninzu;
+        }
+    }
+}
---
+++ kuriage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_10
+{
+    public class kuriage
+    {
+        public static int Kuriage10(int j)
+        {
+            if (j % 10 != 0)
+            {
+                if (j > 0)
+                {
+                    j = j / 10;
+                    j += 1;
+                    j = j * 10;
+                }
+                else if (j == 0)
+                {
+                    j = 0;
+                }
+                else
+                {
+                    j = j / 10;
+                    j -= 1;
+                    j = j * 10;
+                }
+            }
+
+            return j;
+
+        }
+    }
+}
