8ccca8c654e2caed7979aa5ef655091f48de8651
龍成 小林 <190736@jc-21.jp>
2022-02-18 14:10:20 +0900

    ジェットコースタ

--- JetCoaster.cs
+++ JetCoaster.cs
@@ -14,42 +14,53 @@ namespace _12_10
 
             int ninzu = 0; //人数
             int a = k;//現在人数制限
+            int count = 0;
 
             for (int j = 0; ; j += 1)//配列カウント
                 {
-                
-                if(r == 0)//rまで回す
-                {
-                    break;
-                }
-
-                if (j > g.Length - 1)//配列上限まで行ったら最初に戻る
-                {
-                    j = 0;
-                }
-                Console.WriteLine("g"+g[j]);
-                if (g[j] <= a)//人数はいるとき
-                {
-                    ninzu += g[j];
-                    a -= g[j];
-                }
-
-                else//人数はいらないとき
+                count += 1;
+                if (count < g.Length)//同じ人が乗らないように
                 {
 
-                    a = k;
-                    r -= 1;
 
-                    if (j <= g.Length - 1)
+
+                    if (r == 0)//rまで回す
                     {
-                        j -= 1;
+                        break;
                     }
-                    else
+
+                    if (j > g.Length - 1)//配列上限まで行ったら最初に戻る
                     {
-                        j = g.Length - 1;
+                        j = 0;
+                    }
+
+                    if (g.Length == 0)//配列は入っていないとき０
+                    {
+                        return 0;
+                    }
+
+                    if (g[j] <= a)//人数はいるとき
+                    {
+                        ninzu += g[j];
+                        a -= g[j];
+                    }
+
+                    else//人数はいらないとき
+                    {
+
+                        a = k;
+                        r -= 1;
+
+                        if (j <= g.Length - 1)
+                        {
+                            j -= 1;
+                        }
+                        else
+                        {
+                            j = g.Length - 1;
+                        }
                     }
                 }
-
             }
 
              return ninzu;
